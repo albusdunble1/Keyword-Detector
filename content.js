beae2dd088435a1a10ebca465b81847193a01100
Ng Wei Sheng <weisheng_rules@hotmail.com>
2019-08-25 02:56:21 +0800

    added color changing animation

--- content.js
+++ content.js
@@ -3,8 +3,26 @@ keywords = ['lol', 'haha', 'lmao', '?', 'why', 'what', 'how', 'when', 'where']
 setInterval(function(){ 
     for (i = 0; i < keywords.length; i++){
         $(`.text-fragment:contains(${keywords[i]})`).css({
-            "font-size": "2.5em"
+            "font-size": "2.5em",
+            "animation": "color-change 2s infinite"
         });
     }
  }, 100);
+
+//  setInterval(function(){ 
+//     for (i = 0; i < keywords.length; i++){
+//         $(`.text-fragment:contains(${keywords[i]})`).css({
+//             "color": "red"
+//         });
+//     }
+//  }, 500);
+
+
+//  setInterval(function(){ 
+//     for (i = 0; i < keywords.length; i++){
+//         $(`.text-fragment:contains(${keywords[i]})`).css({
+//             "color": "yellow"
+//         });
+//     }
+//  }, 600);
 
