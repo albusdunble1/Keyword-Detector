a38e3fc3b942045b41f64637e7bd64122ae47f8d
Ng Wei Sheng <31728053+albusdunble1@users.noreply.github.com>
2022-04-13 00:17:48 +0800

    Update content.js

--- content.js
+++ content.js
@@ -4,6 +4,15 @@ setInterval(function(){
     for (i = 0; i < keywords.length; i++){
         $(`.text-fragment:contains(${keywords[i]})`).css({
             "font-size": "2.5em",
+            "font-weight": "600",
+            "animation": "color-change 2s infinite"
+        });
+    }
+    
+    for (i = 0; i < keywords.length; i++){
+        $(`.seventv-text-fragment:contains(${keywords[i]})`).css({
+            "font-size": "2.5em",
+            "font-weight": "600",
             "animation": "color-change 2s infinite"
         });
     }
